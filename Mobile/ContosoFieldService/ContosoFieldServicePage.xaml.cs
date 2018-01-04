20a8492606c6de01770184a1fbfe4d933ef2abe9
Mike James <mijam@microsoft.com>
2018-01-04 16:59:56 +0100

    Added Forms Project with FreshMvvm nuget package

---
+++ Mobile/ContosoFieldService/ContosoFieldServicePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ContosoFieldService
+{
+    public partial class ContosoFieldServicePage : ContentPage
+    {
+        public ContosoFieldServicePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
