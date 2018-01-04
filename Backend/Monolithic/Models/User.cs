445536e152092ffb1a2997ee61dd1e2eb265eb26
Mike James <mijam@microsoft.com>
2018-01-04 13:01:47 +0100

    Added base model & user model. Tweaked job model

---
+++ Backend/Monolithic/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ContosoMaintenance.WebAPI.Models
+{
+    public class User : BaseModel
+    {
+        [JsonProperty ("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty ("lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty ("emailAddress")]
+        public string EmailAddress { get; set; }
+
+        [JsonProperty ("cellNumber")]
+        public string CellNumber { get; set; }
+
+
+    }
+}
