829a1e5b5be6f090cac03a47fcc17e10a95a685e
Mike James <mijam@microsoft.com>
2018-01-04 12:50:51 +0100

    Defined basic model for job

---
+++ Backend/Monolithic/Models/Job.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ContosoMaintenance.WebAPI.Models
+{
+    public class Job
+    {
+
+        [JsonProperty ("name")]
+        public string Name { get; set; }
+
+        [JsonProperty ("details")]
+        public string Details { get; set; }
+
+        [JsonProperty("type")]
+        public JobType Type { get; set; }
+
+
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum JobType
+    {
+        Installation,
+        Repair,
+        Service
+    }
+}
