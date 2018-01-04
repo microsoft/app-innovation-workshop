ada07c7e19d5f836fa475d136b54844a8a478bbe
Mike James <mijam@microsoft.com>
2018-01-04 13:39:29 +0100

    Adding Job Route

--- Backend/Monolithic/Controllers/JobController.cs
+++ Backend/Monolithic/Controllers/JobController.cs
@@ -1,9 +1,10 @@
 ﻿using ContosoMaintenance.WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ContosoMaintenance.WebAPI.Controllers
 {
+    [Route("/api/job")]
     public class JobController : BaseController<Job>
     {
-
     }
 }
