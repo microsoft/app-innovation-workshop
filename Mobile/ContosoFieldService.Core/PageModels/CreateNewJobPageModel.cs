3e568e28290408c4a7eac6e17f9db7158e9671dc
Mike James <mijam@microsoft.com>
2018-01-05 11:54:05 +0100

    Creating New Job page

---
+++ Mobile/ContosoFieldService.Core/PageModels/CreateNewJobPageModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using FreshMvvm;
+using Xamarin.Forms;
+using ContosoFieldService.Models;
+using ContosoFieldService.Services;
+
+namespace ContosoFieldService.PageModels
+{
+    public class CreateNewJobPageModel : FreshBasePageModel
+    {
+
+        #region Bindable Properties 
+        public string Name { get; set; }
+        public string Details { get; set; }
+
+        public Command CreateJob
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    var job = new Job();
+                    job.Name = Name;
+                    job.Details = Details;
+
+                    await jobApiService.CreateJobAsync(job);
+                });
+            }
+        }
+
+        public Command CancelCreation
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    await CoreMethods.PopPageModel(true, true);
+                });
+            }
+
+        }
+
+        #endregion
+
+        #region Services
+
+        JobsAPIService jobApiService = new JobsAPIService();
+
+        #endregion
+
+    }
+}
+
---
+++ Mobile/ContosoFieldService.Core/Pages/CreateNewJobPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ContosoFieldService.Pages
+{
+    public partial class CreateNewJobPage : ContentPage
+    {
+        public CreateNewJobPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
