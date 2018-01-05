200b0f81df0a9d189283f564b4075d9c7466f0f1
Mike James <mijam@microsoft.com>
2018-01-05 13:51:17 +0100

    Updating constants

--- Mobile/ContosoFieldService.Core/Helpers/Constants.cs
+++ Mobile/ContosoFieldService.Core/Helpers/Constants.cs
@@ -3,11 +3,12 @@ namespace ContosoFieldService.Helpers
 {
     public static class Constants
     {
-        public const string BaseUrl = "http://contosomaintenance.azurewebsites.net";
+        public const string BaseUrl = "https://contosomaintenance.azure-api.net";
+        public const string ApiManagementKey = "__ApiManagementKey__";
 
         public const string AppCenterIOSKey = "__IOSKey__";
-        public const string AppCenterAndroidKey = "__AndroidKey__ ";
-        public const string AppCenterUWPKey = "__UWPKey__ ";
+        public const string AppCenterAndroidKey = "__AndroidKey__";
+        public const string AppCenterUWPKey = "__UWPKey__";
 
     }
 }
