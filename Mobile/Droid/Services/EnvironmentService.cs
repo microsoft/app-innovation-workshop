885225435e8681f81f01e70ed0009291c0b17639
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-01-08 16:27:05 +0100

    Avoid Visual Studio App Center start when not running in real world scenario. Bug #205

---
+++ Mobile/Droid/Services/EnvironmentService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.OS;
+using ContosoFieldService.Abstractions;
+using ContosoFieldService.Droid.Services;
+
+[assembly: Xamarin.Forms.Dependency(typeof(EnvironmentService))]
+namespace ContosoFieldService.Droid.Services
+{
+    public class EnvironmentService : IEnvironmentService
+    {
+        public bool IsRunningInRealWorld()
+        {
+#if DEBUG
+            return false;
+#endif
+
+            if (Build.Fingerprint.Contains("vbox") || Build.Fingerprint.Contains("generic") ||
+                System.Environment.GetEnvironmentVariable("XAMARIN_TEST_CLOUD") != null)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ Mobile/iOS/Services/EnvironmentService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ContosoFieldService.Abstractions;
+using ContosoFieldService.iOS.Services;
+using ObjCRuntime;
+
+[assembly: Xamarin.Forms.Dependency(typeof(EnvironmentService))]
+namespace ContosoFieldService.iOS.Services
+{
+    public class EnvironmentService : IEnvironmentService
+    {
+        public bool IsRunningInRealWorld()
+        {
+#if DEBUG
+            return false;
+#endif
+
+            if (Runtime.Arch == Arch.SIMULATOR ||
+                Environment.GetEnvironmentVariable("XAMARIN_TEST_CLOUD") != null)
+                return false;
+
+            return true;
+        }
+    }
+}
