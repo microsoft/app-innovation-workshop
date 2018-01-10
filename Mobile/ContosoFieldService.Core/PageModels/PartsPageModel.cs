4bf2276039e413c18bc0d11bb37b9f4b5688ec4f
Mike James <mijam@microsoft.com>
2018-01-10 15:28:56 +0100

    Parting implementing parts API

---
+++ Mobile/ContosoFieldService.Core/PageModels/PartsPageModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ContosoFieldService.Models;
+using FreshMvvm;
+using MvvmHelpers;
+
+namespace ContosoFieldService.PageModels
+{
+    public class PartsPageModel : FreshBasePageModel
+    {
+        public ObservableRangeCollection<Part> Parts { get; set; }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+
+            Parts = new ObservableRangeCollection<Part>();
+        }
+
+
+    }
+}
