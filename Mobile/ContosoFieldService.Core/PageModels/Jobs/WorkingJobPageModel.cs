cf77833a2ecb9d91397138598192d9febe7250f5
Mike James <mijam@microsoft.com>
2018-01-10 13:54:49 +0100

    Implementing camera functionality

--- Mobile/ContosoFieldService.Core/PageModels/Jobs/WorkingJobPageModel.cs
+++ Mobile/ContosoFieldService.Core/PageModels/Jobs/WorkingJobPageModel.cs
@@ -5,17 +5,23 @@ using Xamarin.Forms;
 using Humanizer;
 using System.Timers;
 using System.Threading.Tasks;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using ContosoFieldService.Models;
 
 namespace ContosoFieldService.PageModels
 {
     public class WorkingJobPageModel : FreshBasePageModel
     {
+        Job selectedJob;
         DateTime startedJobTime;
         Timer timer;
         int tenSecondCount;
 
+
         public string Duration { get; set; }
         public string Billable { get; set; }
+        public bool CameraSupported { get => CrossMedia.Current.IsCameraAvailable ? true : false; }
 
         protected override async void ViewIsAppearing(object sender, EventArgs e)
         {
@@ -51,6 +57,32 @@ namespace ContosoFieldService.PageModels
             }
         }
 
+        public Command SnapPhotoClicked
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    if(CrossMedia.Current.IsCameraAvailable == false)
+                    {
+                        await CoreMethods.DisplayAlert("Camera Unavailable", "Unable to use your camera at this time", "OK");
+                        Analytics.TrackEvent("Camera Unavailable");
+                        return;
+                    }
+
+                    var options = new StoreCameraMediaOptions
+                    {
+                        DefaultCamera = CameraDevice.Rear,
+                        SaveMetaData = true,
+                        SaveToAlbum = true,
+                        Name = selectedJob.Name + DateTime.Now,
+                    };
+               
+                    Analytics.TrackEvent("Taking a photo");
+                    await CrossMedia.Current.TakePhotoAsync(options);
+                });
+            }
+        }
 
     }
 }
