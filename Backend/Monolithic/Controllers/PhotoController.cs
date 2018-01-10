2bae00a403731f2aa7e043138850844db8e381fc
Mike James <mijam@microsoft.com>
2018-01-11 00:01:55 +0100

    Added new controllers to web api

---
+++ Backend/Monolithic/Controllers/PhotoController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoMaintenance.WebAPI.Controllers
+{
+    [Route("/api/photo")]
+    public class PhotoController : Controller
+    {
+    }
+}
