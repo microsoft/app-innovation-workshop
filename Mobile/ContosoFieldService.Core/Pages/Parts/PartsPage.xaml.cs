74ef4652cf94b14b22255ab3950327c8d18668e5
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-01-11 18:27:13 +0100

    Add Parts page

--- Mobile/ContosoFieldService.Core/Pages/Parts/PartsPage.xaml.cs
+++ Mobile/ContosoFieldService.Core/Pages/Parts/PartsPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 using Xamarin.Forms;
 
-namespace ContosoFieldService.Pages.Parts
+namespace ContosoFieldService.Pages
 {
     public partial class PartsPage : ContentPage
     {
