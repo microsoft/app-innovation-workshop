951b202b3d75081fd753cd72cbafe5924a01f31c
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-01-11 18:27:13 +0100

    Add Parts page

---
+++ Mobile/ContosoFieldService.Core/PageModels/Parts/PartDetailsPageModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using FreshMvvm;
+
+namespace ContosoFieldService.PageModels
+{
+    public class PartDetailsPageModel : FreshBasePageModel
+    {
+        public PartDetailsPageModel()
+        {
+        }
+    }
+}
