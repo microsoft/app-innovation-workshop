93e9df4c76b63fff7650e1e1c384fc20139cab9f
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-01-11 18:39:02 +0100

    Add part details

--- Mobile/ContosoFieldService.Core/PageModels/Parts/PartDetailsPageModel.cs
+++ Mobile/ContosoFieldService.Core/PageModels/Parts/PartDetailsPageModel.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using FreshMvvm;
+using ContosoFieldService.Models;
+using Xamarin.Forms;
 
 namespace ContosoFieldService.PageModels
 {
     public class PartDetailsPageModel : FreshBasePageModel
     {
-        public PartDetailsPageModel()
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string ModelNumber { get; set; }
+        public string SerialNumber { get; set; }
+        public string PartNumber { get; set; }
+        public decimal PriceInUSD { get; set; }
+        public string ImageSource { get; set; }
+
+        Part Part;
+
+        public override void Init(object initData)
         {
+            if (initData != null)
+            {
+
+                Part = (Part)initData;
+                Name = Part.Name;
+                Manufacturer = Part.Manufacturer;
+                ModelNumber = Part.ModelNumber;
+                SerialNumber = Part.SerialNumber;
+                PartNumber = Part.PartNumber;
+                PriceInUSD = Part.PriceInUSD;
+                ImageSource = Part.ImageSource;
+            }
+            else
+            {
+                Part = new Part();
+            }
+        }
+
+        public Command StartJobClicked
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    //await CoreMethods.PushPageModel<WorkingJobPageModel>(null, true, true);
+                });
+            }
         }
     }
 }
