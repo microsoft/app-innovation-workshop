d7dbc50ccd83080e8f835ee49957075cd07d771d
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-01-12 01:04:21 +0100

    Add resize function

--- Backend/Functions/ContosoMaintenance.ResizeFunction/ResizeImage.cs
+++ Backend/Functions/ContosoMaintenance.ResizeFunction/ResizeImage.cs
@@ -1,47 +1,109 @@
 using System.Collections.Generic;
-using Microsoft.Azure.Documents;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using System.IO;
-using Microsoft.WindowsAzure.Storage.Blob;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+using Newtonsoft.Json;
+using Microsoft.Azure.Documents;
 
 namespace ContosoMaintenance.Functions
 {
     public static class ResizeImage
     {
-        [FunctionName("ResizeImage")]
-        public static void Run(
+        [FunctionName("ProcessPhotosQueue")]
+        public static async Task Run(
             // Trigger
-            [CosmosDBTrigger("contosomaintenance", "jobs", ConnectionStringSetting = "contosomaintenancedb_connectionstring", CreateLeaseCollectionIfNotExists = true)] IReadOnlyList<Document> documents,
+            [QueueTrigger("processphotos")] PhotoProcess queueItem,
 
-            // Input
-            [Blob("images/large", FileAccess.Read)] CloudBlobDirectory imagesLarge,
+            // Inputs
+            [DocumentDB("contosomaintenance", "jobs", Id = "{jobId}", ConnectionStringSetting = "CosmosDb")] Job job,
+            [Blob("images-large/{photoId}.jpg", FileAccess.Read)] byte[] imageLarge,
 
-            // Output
-            [Blob("images/medium", FileAccess.Write)] CloudBlobDirectory imagesMedium,
-            [Blob("images/small", FileAccess.Write)] CloudBlobDirectory imagesSmall,
-
+            // Outputs
+            [Blob("images-medium/{photoId}.jpg", FileAccess.Write)] Stream imageMedium,
+            [Blob("images-icon/{photoId}.jpg", FileAccess.Write)] Stream imageIcon,
+            
             // Logger
             TraceWriter log)
         {
-            log.Info("Documents modified " + documents.Count);
-            log.Info("First document Id " + documents[0].Id);
+            log.Info($"New photo upload detected for job {job.Id}");
 
-            foreach (var document in documents)
+            // Crop photos smart using Microsoft Cognitive Services
+            await CropImageSmartAsync(imageLarge, imageMedium, 300, 300);
+            await CropImageSmartAsync(imageLarge, imageIcon, 150, 150);
+            log.Info("Images cropped");
+
+            // Update Cosmos DB entry
+            var photo = (job.Photos).FirstOrDefault(p => p.Id.Equals(queueItem.PhotoId));
+            if (photo != null)
             {
-                // Parse document to Job
+                photo.MediumUrl = photo.LargeUrl?.Replace("large", "medium");
+                photo.IconUrl = photo.LargeUrl?.Replace("large", "icon");
+                log.Info("Cosmos DB entry updated");
+            }            
+        }
 
-                // Check for attachements
-
-                // Check if resizing is needed
-
-                // Send image, that need resizing to Cognitive Services Computer Vision
-
-                // Save images to according folder
-
-                // Update Job
-
+        private static async Task CropImageSmartAsync(byte[] inputImage, Stream outputImage, int width, int height)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", Environment.GetEnvironmentVariable("Ocp-Apim-Subscription-Key"));
+            var url =$"https://northeurope.api.cognitive.microsoft.com/vision/v1.0/generateThumbnail?width={width}&height={height}&smartCropping=true";
+            using (ByteArrayContent content = new ByteArrayContent(inputImage))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                var resizedImage = await response.Content.ReadAsStreamAsync();
+                resizedImage.CopyTo(outputImage);
             }
         }
+    }
+
+    public class PhotoProcess
+    {
+        [JsonProperty("jobId")]
+        public string JobId { get; set; }
+        [JsonProperty("photoId")]
+        public string PhotoId { get; set; }
+    }
+
+    public class Job
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("createdAt")]
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+
+        public string Details { get; set; }
+        public string Type { get; set; }
+
+        public string Status { get; set; }
+
+        public dynamic Customer { get; set; }
+
+        public string[] Attachements { get; set; }
+
+        public dynamic Address { get; set; }
+
+        public dynamic AssignedTo { get; set; }    
+
+        [JsonProperty("photos")]
+        public List<Photo> Photos { get; set; }
+    }
+
+    public class Photo
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("largeUrl")]
+        public string LargeUrl { get; set; }
+        [JsonProperty("mediumUrl")]
+        public string MediumUrl { get; set; }
+        [JsonProperty("iconUrl")]
+        public string IconUrl { get; set; }
     }
 }
