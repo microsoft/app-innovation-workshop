a769987df98952a87cc2a1a97d6de66b14d0c830
Mike James <mijam@microsoft.com>
2018-01-16 10:17:09 +0100

    Adding icons & Dummy data

--- Backend/Monolithic/Controllers/BaseController.cs
+++ Backend/Monolithic/Controllers/BaseController.cs
@@ -14,6 +14,11 @@ namespace ContosoMaintenance.WebAPI.Controllers
             DBRepository.Initialize();
         }
 
+        void CreateDummyDataIfNeeded()
+        {
+            
+        }
+
         [HttpGet]
         public virtual async Task<IActionResult> GetAll()
         {
