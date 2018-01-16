78bc010d11f27a861c05689f064b8502e6f8a9b3
Michael Sivers <michael@sivers.co.uk>
2018-01-16 23:59:42 +0000

    Add new admin web app + created a 'theme'
    Added an ASP.NET MVC web app to the solution and then created a UI (CSS) in keeping with the mobile design.

---
+++ WebApps/AdminWebApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AdminWebApp.Models;
+
+namespace AdminWebApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
