f4c6e5be0c244d82c72cdd0411321e5357de0fc5
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-01-19 01:29:42 +0100

    Adding Cosmos DB Points

--- Mobile/ContosoFieldService.Core/Pages/Jobs/JobDetailsPage.xaml.cs
+++ Mobile/ContosoFieldService.Core/Pages/Jobs/JobDetailsPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Maps;
+using ContosoFieldService.PageModels;
 
 namespace ContosoFieldService.Pages
 {
@@ -12,6 +14,32 @@ namespace ContosoFieldService.Pages
         public JobDetailsPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var pageModel = BindingContext as JobDetailsPageModel;
+            if (pageModel.Point != null)
+            {
+                var pos = new Position(pageModel.Point.Position.Latitude, pageModel.Point.Position.Longitude);
+
+                // Move map to point
+                mapView.MoveToRegion(MapSpan.FromCenterAndRadius(pos, Distance.FromMiles(1)));
+
+                // Add pin
+                mapView.Pins.Add(new Pin
+                {
+                    Label = "Position",
+                    Type = PinType.Place,
+                    Position = pos
+                });
+            }
+            else
+            {
+                //TODO: Hide map when no point is specified
+            }
         }
     }
 }
