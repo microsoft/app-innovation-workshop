117e7585a8066f9114dad3052e71a80580493a40
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-01-23 22:37:09 -0800

    Styled Jobs Page

--- Mobile/iOS/Renderers/SelectedViewCellRenderer.cs
+++ Mobile/iOS/Renderers/SelectedViewCellRenderer.cs
@@ -4,7 +4,7 @@ using Xamarin.Forms.Platform.iOS;
 using UIKit;
 using ContosoFieldService.iOS.Renderers;
 
-[assembly: ExportRenderer(typeof(ViewCell), typeof(SelectedViewCellRenderer))]
+//[assembly: ExportRenderer(typeof(ViewCell), typeof(SelectedViewCellRenderer))]
 namespace ContosoFieldService.iOS.Renderers
 {
     public class SelectedViewCellRenderer : ViewCellRenderer
