c3ac46e1ed3e8d073aa722210689f62153c71e4b
Mike James <mike@xamarin.com>
2018-01-24 20:12:16 -0800

    Changing values for CI/CD

--- Mobile/ContosoFieldService.Core/Helpers/Constants.cs
+++ Mobile/ContosoFieldService.Core/Helpers/Constants.cs
@@ -2,11 +2,11 @@
 {
     public static class Constants
     {
-        public const string BaseUrl = "https://contosomaintenance.azure-api.net";
-        public const string ApiManagementKey = "Ocp-Apim-Subscription-Key: a4c54ce4d010428e8d97a540b50f27c9";
+        public const string BaseUrl = "__ApiManagementURL__";
+        public const string ApiManagementKey = "__ApiManagementKey__";
 
-        public const string AppCenterIOSKey = "ios=d549df18-9f1c-4dd1-ab9a-8b677471686d;";
-        public const string AppCenterAndroidKey = "android=d9d6170d-5b81-44c5-bf30-02608e4352bb;";
-        public const string AppCenterUWPKey = "";
+        public const string AppCenterIOSKey = "__IOSKey__";
+        public const string AppCenterAndroidKey = "__AndroidKey__ ";
+        public const string AppCenterUWPKey = "__UWPKey__ ";
     }
 }
