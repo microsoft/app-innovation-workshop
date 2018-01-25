275953075ca3821d24055ff4b17a78753149583d
Mike James <mike@xamarin.com>
2018-01-24 20:44:14 -0800

    Editing Job to include DueDate (bug fix)

--- Backend/Monolithic/Models/Job.cs
+++ Backend/Monolithic/Models/Job.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Azure.Search;
 using Microsoft.Azure.Search.Models;
 using Newtonsoft.Json;
@@ -25,6 +26,8 @@ namespace ContosoMaintenance.WebAPI.Models
         public Location Address { get; set; }
 
         public Employee AssignedTo { get; set; }
+
+        public DateTime DueDate { get; set; }
 
     }
 
