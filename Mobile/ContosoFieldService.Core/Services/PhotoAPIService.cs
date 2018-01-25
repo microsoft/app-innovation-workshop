8dc651d3c67061cde4ebfc120a24b17234e19c89
Mike James <mike@xamarin.com>
2018-01-24 20:46:32 -0800

    Refactoring PhotoAPIService

--- Mobile/ContosoFieldService.Core/Services/PhotoAPIService.cs
+++ Mobile/ContosoFieldService.Core/Services/PhotoAPIService.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using ContosoFieldService.Models;
+﻿using System.Threading.Tasks;
 using Plugin.Media.Abstractions;
 using Refit;
 
