44e08ce9e6fb03afcc49d8e0853af68aab239d68
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-01-25 13:30:54 -0800

    Temporarily enable App Center for Debug

--- Mobile/Droid/Services/EnvironmentService.cs
+++ Mobile/Droid/Services/EnvironmentService.cs
@@ -11,7 +11,7 @@ namespace ContosoFieldService.Droid.Services
         public bool IsRunningInRealWorld()
         {
 #if DEBUG
-            return false;
+            //return false;
 #endif
 
             if (Build.Fingerprint.Contains("vbox") || Build.Fingerprint.Contains("generic") ||
