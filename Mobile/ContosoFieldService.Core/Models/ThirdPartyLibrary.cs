09f0bdd5e7155e8334cc51802d91624c575d4728
Mike James <mike@xamarin.com>
2018-01-25 13:23:06 -0800

    Started tweaking the settings page

---
+++ Mobile/ContosoFieldService.Core/Models/ThirdPartyLibrary.cs
@@ -0,0 +1,16 @@
+﻿namespace ContosoFieldService.Models
+{
+    public class ThirdPartyLibrary
+    {
+        public ThirdPartyLibrary(string name, string url, string author)
+        {
+            Name = name;
+            Url = url;
+            Author = author;
+        }
+
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Author { get; set; }
+    }
+}
--- Mobile/ContosoFieldService.Core/PageModels/Profile/SettingsPageModel.cs
+++ Mobile/ContosoFieldService.Core/PageModels/Profile/SettingsPageModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using ContosoFieldService.Models;
 using FreshMvvm;
 using Microsoft.AppCenter.Push;
 
@@ -7,6 +9,8 @@ namespace ContosoFieldService.PageModels
     public class SettingsPageModel : FreshBasePageModel
     {
         bool notificationsEnabled; 
+        public ObservableCollection<ThirdPartyLibrary> ThirdPartyLibraries;
+
         public bool NotificationsEnabled
         {
             get
@@ -24,6 +28,16 @@ namespace ContosoFieldService.PageModels
         {
             base.Init(initData);
             NotificationsEnabled = await Push.IsEnabledAsync();
+
+            ThirdPartyLibraries = new ObservableCollection<ThirdPartyLibrary>();
+            ThirdPartyLibraries.Add(new ThirdPartyLibrary("CarouselView", "Alex Rainman", "https://github.com/alexrainman/CarouselView"));
+            ThirdPartyLibraries.Add(new ThirdPartyLibrary("Lottie", "AirBnB, Martijn van Dijk", "https://github.com/martijn00/LottieXamarin"));
+            ThirdPartyLibraries.Add(new ThirdPartyLibrary("Corcav.Behaviors", "Corrado Cavalli", "https://github.com/corradocavalli/Corcav.Behaviors"));
+            ThirdPartyLibraries.Add(new ThirdPartyLibrary("Forms Toolkit", "James Montemagno", "https://github.com/jamesmontemagno/xamarin.forms-toolkit"));
+            ThirdPartyLibraries.Add(new ThirdPartyLibrary("FreshMvvm", "Michael Ridland", "https://github.com/rid00z/FreshMvvm"));
+            ThirdPartyLibraries.Add(new ThirdPartyLibrary("Humanizer", "Mehdi Khalili", "https://github.com/Humanizr/Humanizer"));
+            ThirdPartyLibraries.Add(new ThirdPartyLibrary("Refit", "Paul Betts", "https://github.com/paulcbetts/refit"));
+            ThirdPartyLibraries.Add(new ThirdPartyLibrary("MvvmHelpers", "James Montemagno", "https://github.com/jamesmontemagno/mvvm-helpers"));
         }
     }
 }
