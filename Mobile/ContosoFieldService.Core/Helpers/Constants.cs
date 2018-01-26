412a2a4e27632bd21226018a3d97ab2d9360f875
Mike James <michael.james6@gmail.com>
2018-01-25 16:55:30 -0800

    Resolved space issue causing the app to crash when deployed through CI/CD

--- Mobile/ContosoFieldService.Core/Helpers/Constants.cs
+++ Mobile/ContosoFieldService.Core/Helpers/Constants.cs
@@ -6,7 +6,7 @@
         public const string ApiManagementKey = "__ApiManagementKey__";
 
         public const string AppCenterIOSKey = "__IOSKey__";
-        public const string AppCenterAndroidKey = "__AndroidKey__ ";
-        public const string AppCenterUWPKey = "__UWPKey__ ";
+        public const string AppCenterAndroidKey = "__AndroidKey__";
+        public const string AppCenterUWPKey = "__UWPKey__";
     }
 }
