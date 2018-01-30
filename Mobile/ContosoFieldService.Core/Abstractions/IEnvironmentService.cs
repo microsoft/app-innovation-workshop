687e024b60d11ffb3c21f13e7d887251eb982495
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-01-30 21:03:44 +0100

    A bit of refactoring and comments

--- Mobile/ContosoFieldService.Core/Abstractions/IEnvironmentService.cs
+++ Mobile/ContosoFieldService.Core/Abstractions/IEnvironmentService.cs
@@ -1,9 +1,17 @@
-﻿using System;
-
-namespace ContosoFieldService.Abstractions
+﻿namespace ContosoFieldService.Abstractions
 {
+    /// <summary>
+    /// Checks the current environment. Has be be implemented by each platform
+    /// </summary>
     public interface IEnvironmentService
     {
+        /// <summary>
+        /// Checks if app is running on a real device by a user.
+        /// </summary>
+        /// <returns>
+        ///     <c>false</c>, if running on a Simulator/Emulator, in Debug Mode or a in App Center Test Cloud, 
+        ///     <c>true</c> otherwise.
+        /// </returns>
         bool IsRunningInRealWorld();
     }
 }
---
+++ Mobile/ContosoFieldService.Core/Helpers/PageModelLocator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using ContosoFieldService.Helpers;
+using ContosoFieldService.PageModels;
+using Xamarin.Forms;
+
+namespace ContosoFieldService
+{
+    /// <summary>
+    /// Used to display dummy data at design time
+    /// </summary>
+    public static class PageModelLocator
+    {
+        static JobsPageModel jobsPageModel;
+        public static JobsPageModel JobsPageModel => jobsPageModel ?? (jobsPageModel = new JobsPageModel { Jobs = DummyData.GetGroupedDummyJobs() });
+        static JobDetailsPageModel jobDetailsPageModel;
+        public static JobDetailsPageModel JobDetailsPageModel
+        {
+            get
+            {
+                if (jobDetailsPageModel == null)
+                {
+                    jobDetailsPageModel = new JobDetailsPageModel();
+                    jobDetailsPageModel.Init(DummyData.GetDummyJobs().First());
+                }
+
+                return jobDetailsPageModel;
+            }
+        }
+
+        static PartsPageModel partsPageModel;
+        public static PartsPageModel PartsPageModel => partsPageModel ?? (partsPageModel = new PartsPageModel { Parts = DummyData.GetDummyParts() });
+        static PartDetailsPageModel partDetailsPageModel;
+        public static PartDetailsPageModel PartDetailsPageModel
+        {
+            get
+            {
+                if (partDetailsPageModel == null)
+                {
+                    partDetailsPageModel = new PartDetailsPageModel();
+                    partDetailsPageModel.Init(DummyData.GetDummyParts().First());
+                }
+
+                return partDetailsPageModel;
+            }
+        }
+
+    }
+}
+
