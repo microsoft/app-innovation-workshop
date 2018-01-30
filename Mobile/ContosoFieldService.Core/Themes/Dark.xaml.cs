0a31e46a3f5ba681373ec7a890a88b59110fc05f
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-01-29 19:32:01 -0800

    Add Dark and Light Themes

---
+++ Mobile/ContosoFieldService.Core/Themes/Dark.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ContosoFieldService.Themes
+{
+    public partial class Dark : ResourceDictionary
+    {
+        public Dark()
+        {
+            InitializeComponent();
+        }
+    }
+}
