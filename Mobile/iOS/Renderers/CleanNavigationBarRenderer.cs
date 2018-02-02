54d13c2e4343069216da74c3d8d53454622de08a
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-02-02 11:52:29 +0100

    Add a bunch of styling

---
+++ Mobile/iOS/Renderers/CleanNavigationBarRenderer.cs
@@ -0,0 +1,30 @@
+﻿using ContosoFieldService.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ContentPage), typeof(CleanNavigationBarRenderer))]
+namespace ContosoFieldService.iOS.Renderers
+{
+    public class CleanNavigationBarRenderer : PageRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            UINavigationBar.Appearance.SetTitleTextAttributes(new UITextAttributes
+            {
+                Font = UIFont.FromName("MuseoSans-500", 18),
+                TextColor = UIColor.White
+            });
+
+            // Remove transparency, shadow and separator line
+            UINavigationBar.Appearance.Translucent = false;
+            UINavigationBar.Appearance.ShadowImage = new UIImage();
+            UINavigationBar.Appearance.SetBackgroundImage(new UIImage(), UIBarMetrics.Default);
+
+            // White Status Bar
+            UIApplication.SharedApplication.SetStatusBarStyle(UIStatusBarStyle.LightContent, false);
+        }
+    }
+}
---
+++ Mobile/iOS/Renderers/CleanTabBarRenderer.cs
@@ -0,0 +1,23 @@
+﻿using ContosoFieldService.iOS.Renderers;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(CleanTabbedRenderer))]
+namespace ContosoFieldService.iOS.Renderers
+{
+    public class CleanTabbedRenderer : TabbedRenderer
+    {
+        public CleanTabbedRenderer()
+        {
+            TabBar.Translucent = false;
+            //TabBar.Opaque = true;
+
+            // Remove shadow and separator line
+            TabBar.BackgroundImage = new UIImage();
+            TabBar.ShadowImage = new UIImage();
+        }
+
+    }
+}
