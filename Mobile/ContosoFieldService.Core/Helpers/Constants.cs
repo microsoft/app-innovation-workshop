7ebf5206f475f69e06fe58bbec4bb75ff0abc569
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-02-27 15:42:38 +0100

    Replace Constants with placeholders

--- Mobile/ContosoFieldService.Core/Helpers/Constants.cs
+++ Mobile/ContosoFieldService.Core/Helpers/Constants.cs
@@ -1,12 +1,12 @@
-﻿namespace ContosoFieldService.Helpers
+namespace ContosoFieldService.Helpers
 {
     public static class Constants
     {
-        public const string BaseUrl = "https://contosomaintenance.azure-api.net";
-        public const string ApiManagementKey = "Ocp-Apim-Subscription-Key: a4c54ce4d010428e8d97a540b50f27c9";
+        public const string BaseUrl = "__ApiManagementURL__";
+        public const string ApiManagementKey = "__ApiManagementKey__";
 
-        public const string AppCenterIOSKey = "ios=0f5e85f5-7774-4fbd-ab6c-f1f018845ce2;";
-        public const string AppCenterAndroidKey = "android=d08e7d3d-a10b-40c4-93bf-008a53c8a35e;";
-        public const string AppCenterUWPKey = "";
+        public const string AppCenterIOSKey = "__IOSKey__";
+        public const string AppCenterAndroidKey = "__AndroidKey__";
+        public const string AppCenterUWPKey = "__UWPKey__";
     }
 }
