609754d1343756399dc4d02c8b3074bdc8a41acf
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-03-20 21:44:18 +0100

    More work on auth

---
+++ Mobile/ContosoFieldService.Core/Services/BaseAPIService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ContosoFieldService.Abstractions;
+
+namespace ContosoFieldService.Services
+{
+    public abstract class BaseAPIService
+    {
+        IAuthenticationService authenticationService;
+
+
+        public BaseAPIService(IAuthenticationService authenticationService)
+        {
+            this.authenticationService = authenticationService;
+        }
+    }
+}
