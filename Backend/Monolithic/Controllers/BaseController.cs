73ac7065e9c7f0bc7cc848f629cc36ad34ce9fd1
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-03-23 13:39:11 +0100

    Fix Android Authentication bug #22

--- Backend/Monolithic/Controllers/BaseController.cs
+++ Backend/Monolithic/Controllers/BaseController.cs
@@ -9,94 +9,96 @@ using System.Security.Claims;
 
 namespace ContosoMaintenance.WebAPI.Controllers
 {
-    public class BaseController<T> : Controller where T : Models.BaseModel
-    {
-        public DocumentDBRepositoryBase<T> DBRepository = new DocumentDBRepositoryBase<T>();
+	public class BaseController<T> : Controller where T : Models.BaseModel
+	{
+		public DocumentDBRepositoryBase<T> DBRepository = new DocumentDBRepositoryBase<T>();
 
-        public BaseController(IConfiguration configuration)
-        {
-            // Initialize Azure Cosmos DB instance for this controller
-            DBRepository.Initialize(
-                configuration["AzureCosmosDb:Endpoint"],
-                configuration["AzureCosmosDb:Key"],
-                configuration["AzureCosmosDb:DatabaseId"]);
-        }
+		public BaseController(IConfiguration configuration)
+		{
+			// Initialize Azure Cosmos DB instance for this controller
+			DBRepository.Initialize(
+				configuration["AzureCosmosDb:Endpoint"],
+				configuration["AzureCosmosDb:Key"],
+				configuration["AzureCosmosDb:DatabaseId"]);
+		}
 
-        [HttpGet]
-        public virtual async Task<IActionResult> GetAll()
-        {
-            var items = await DBRepository.GetItemsAsync(x => x.Id != null && x.IsDeleted != true);
-            return new ObjectResult(items);
-        }
+		[HttpGet]
+		public virtual async Task<IActionResult> GetAll()
+		{
+			var items = await DBRepository.GetItemsAsync(x => x.Id != null && x.IsDeleted != true);
+			return new ObjectResult(items);
+		}
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(string id)
-        {
-            if (string.IsNullOrEmpty(id) == true)
-                return BadRequest();
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetById(string id)
+		{
+			if (string.IsNullOrEmpty(id) == true)
+				return BadRequest();
 
-            var items = await DBRepository.GetItemsAsync(x => x.Id == id);
-            return new ObjectResult(items);
-        }
+			var items = await DBRepository.GetItemsAsync(x => x.Id == id);
+			return new ObjectResult(items);
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Create([FromBody] T item)
-        {
-            if (item == null)
-                return BadRequest();
+		[HttpPost]
+		public async Task<IActionResult> Create([FromBody] T item)
+		{
+			if (item == null)
+				return BadRequest();
 
-            if (string.IsNullOrEmpty(item.Id))
-                item.Id = Guid.NewGuid().ToString();
+			if (string.IsNullOrEmpty(item.Id))
+				item.Id = Guid.NewGuid().ToString();
 
-            await DBRepository.CreateItemAsync(item);
-            return new ObjectResult(item);
-        }
+			await DBRepository.CreateItemAsync(item);
+			return new ObjectResult(item);
+		}
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Update(string id, [FromBody] T item)
-        {
-            if (item == null || item.Id != id)
-            {
-                return BadRequest();
-            }
+		[HttpPut("{id}")]
+		public async Task<IActionResult> Update(string id, [FromBody] T item)
+		{
+			if (item == null || item.Id != id)
+			{
+				return BadRequest();
+			}
 
-            var headline = await DBRepository.GetItemAsync(item.Id);
-            if (headline == null)
-            {
-                return NotFound();
-            }
+			var headline = await DBRepository.GetItemAsync(item.Id);
+			if (headline == null)
+			{
+				return NotFound();
+			}
 
-            await DBRepository.UpdateItemAsync(item.Id, item);
-            return new ObjectResult(item);
-        }
+			await DBRepository.UpdateItemAsync(item.Id, item);
+			return new ObjectResult(item);
+		}
 
-        [Authorize]
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> DeleteAsync(string id)
-        {
-            // Get ID of user who sends the request
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+		[Authorize(Roles = "Admin")]
+		[HttpDelete("{id}")]
+		public async Task<ActionResult> DeleteAsync(string id)
+		{
+			// Get ID of user who sends the request
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            // TODO: Check, if user is allowed to delete item
-            // Currently left out for demo reasons
 
-            if (id == null)
-            {
-                return BadRequest();
-            }
 
-            T item = await DBRepository.GetItemAsync(id);
-            if (item == null)
-            {
-                return NotFound();
-            }
+			// TODO: Check, if user is allowed to delete item
+			// Currently left out for demo reasons
 
-            // Soft Delete
-            item.IsDeleted = true;
-            await DBRepository.UpdateItemAsync(item.Id, item);
-            //await DBRepository.DeleteItemAsync(id);
+			if (id == null)
+			{
+				return BadRequest();
+			}
 
-            return new ObjectResult(item);
-        }
-    }
+			T item = await DBRepository.GetItemAsync(id);
+			if (item == null)
+			{
+				return NotFound();
+			}
+
+			// Soft Delete
+			item.IsDeleted = true;
+			await DBRepository.UpdateItemAsync(item.Id, item);
+			//await DBRepository.DeleteItemAsync(id);
+
+			return new ObjectResult(item);
+		}
+	}
 }
--- Mobile/Droid/MainActivity.cs
+++ Mobile/Droid/MainActivity.cs
@@ -20,35 +20,41 @@ using Microsoft.Identity.Client;
 
 namespace ContosoFieldService.Droid
 {
-    [Activity(Label = "Field Service", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+	[Activity(Label = "Field Service", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(bundle);
+			base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-            CachedImageRenderer.Init(false);
-            //ImageCircleRenderer.Init();
-            AnimationViewRenderer.Init();
-            FormsMaps.Init(this, bundle);
-            CarouselViewRenderer.Init();
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+			CachedImageRenderer.Init(false);
+			//ImageCircleRenderer.Init();
+			AnimationViewRenderer.Init();
+			FormsMaps.Init(this, bundle);
+			CarouselViewRenderer.Init();
 
-            // Configure App Center Push
-            Push.SetSenderId("597659151602");
+			// Configure App Center Push
+			Push.SetSenderId("597659151602");
 
-            // Configure Authentication
-            AuthenticationService.UIParent = new UIParent(Xamarin.Forms.Forms.Context as Activity);
+			// Configure Authentication
+			AuthenticationService.UIParent = new UIParent(Xamarin.Forms.Forms.Context as Activity);
 
-            LoadApplication(new App());
-        }
+			LoadApplication(new App());
+		}
 
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
-        {
-            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+		{
+			PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+
+		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+		{
+			base.OnActivityResult(requestCode, resultCode, data);
+			AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
+		}
+	}
 }
