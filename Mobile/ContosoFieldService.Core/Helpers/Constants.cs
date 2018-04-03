08216418ba088f54fd36573a4d5a5e8771369512
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-04-03 16:31:35 +0200

    Reset Mobile App Constants

--- Mobile/ContosoFieldService.Core/Helpers/Constants.cs
+++ Mobile/ContosoFieldService.Core/Helpers/Constants.cs
@@ -2,17 +2,19 @@ namespace ContosoFieldService.Helpers
 {
     public static class Constants
     {
-        public static string BaseUrl = "http://contosomaintenance.azurewebsites.net/api/";
+        // Backend API
+        public static string BaseUrl = "__ApiManagementURL__";
         public static string ApiManagementKey = "__ApiManagementKey__";
 
-        public static string AppCenterIOSKey = "0f5e85f5-7774-4fbd-ab6c-f1f018845ce2";
-        public static string AppCenterAndroidKey = "d08e7d3d-a10b-40c4-93bf-008a53c8a35e";
+        // Visual Studio App Center
+        public static string AppCenterIOSKey = "__IOSKey__";
+        public static string AppCenterAndroidKey = "__AndroidKey__";
         public static string AppCenterUWPKey = "__UWPKey__";
 
         // Azure Active Directory B2C
-        public static string Tenant = "myawesomenewstartup.onmicrosoft.com"; // __ADB2CTenant__
-        public static string ApplicationId = "4844ddcc-786e-460a-a2f5-873dcc649207"; // __ADB2CClientId__
-        public static string SignUpAndInPolicy = "B2C_1_GenericSignUpSignIn"; // __ADB2CSignUpPolicy__
-        public static string[] Scopes = { "https://myawesomenewstartup.onmicrosoft.com/backend/read_only" }; // __ADB2CScopes__
+        public static string Tenant = "__ADB2CTenant__";
+        public static string ApplicationId = "__ADB2CApplicationId__";
+        public static string SignUpAndInPolicy = "__ADB2CSignUpPolicy__";
+        public static string[] Scopes = { "__ADB2CScopes__" };
     }
 }
