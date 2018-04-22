d659fc4d09e790ed514d311e2b5a00ba5aadd79d
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-04-22 12:04:15 +0300

    Add resilient networking to Parts Service on Mobile

--- Mobile/ContosoFieldService.Core/Services/PartsAPIService.cs
+++ Mobile/ContosoFieldService.Core/Services/PartsAPIService.cs
@@ -3,6 +3,10 @@ using System.Threading.Tasks;
 using ContosoFieldService.Models;
 using Refit;
 using ContosoFieldService.Helpers;
+using Plugin.Connectivity;
+using MonkeyCache.FileStore;
+using System.Linq;
+using System;
 
 namespace ContosoFieldService.Services
 {
@@ -18,13 +22,13 @@ namespace ContosoFieldService.Services
         Task<List<Part>> SearchParts(string keyword, [Header("Ocp-Apim-Subscription-Key")] string apiManagementKey);
 
         [Post("/part/")]
-        Task<Part> CreatePart([Body] Part job, [Header("Ocp-Apim-Subscription-Key")] string apiManagementKey);
+        Task<Part> CreatePart([Body] Part part, [Header("Ocp-Apim-Subscription-Key")] string apiManagementKey);
 
         [Post("/part/{id}/")]
         Task<Part> DeletePart(string id, [Header("Authorization")] string authorization, [Header("Ocp-Apim-Subscription-Key")] string apiManagementKey);
     }
 
-    public class PartsAPIService
+    public class PartsAPIService : BaseAPIService
     {
         public async Task<Part> CreatePartAsync(Part part)
         {
@@ -32,10 +36,37 @@ namespace ContosoFieldService.Services
             return await contosoMaintenanceApi.CreatePart(part, Constants.ApiManagementKey);
         }
 
-        public async Task<List<Part>> GetPartsAsync()
+        public async Task<List<Part>> GetPartsAsync(bool force = false)
         {
+            var key = "Parts";
+
+            // Handle online/offline scenario
+            if (!CrossConnectivity.Current.IsConnected && Barrel.Current.Exists(key))
+            {
+                // If no connectivity, we'll return the cached list.
+                return Barrel.Current.Get<List<Part>>(key);
+            }
+
+            // If the data isn't too old, we'll go ahead and return it rather than call the backend again.
+            if (!force && !Barrel.Current.IsExpired(key) && Barrel.Current.Exists(key))
+            {
+                var parts = Barrel.Current.Get<IEnumerable<Part>>(key);
+                return parts.ToList();
+            }
+
+            // Create an instance of the Refit RestService for the part interface.
             var contosoMaintenanceApi = RestService.For<IPartsServiceAPI>(Helpers.Constants.BaseUrl);
-            return await contosoMaintenanceApi.GetParts(Constants.ApiManagementKey);
+
+            // Use Polly to handle retrying
+            var pollyResult = await Policy.ExecuteAndCaptureAsync(async () => await contosoMaintenanceApi.GetParts(Constants.ApiManagementKey));
+            if (pollyResult.Result != null)
+            {
+                // Save parts into the cache
+                Barrel.Current.Add(key, pollyResult.Result, TimeSpan.FromMinutes(5));
+                return pollyResult.Result;
+            }
+
+            return null;
         }
 
         public async Task<Part> GetPartByIdAsync(string id)
