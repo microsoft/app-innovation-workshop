3cc4efb2e7258e77a895a0aae79ea86e9537ff5f
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-06-06 16:38:07 +0200

    Add first two simple UI Tests

---
+++ Mobile/UITests/PageObjects/JobsPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.UITest.Queries;
+
+namespace ContosoFieldService.UITests.PageObjects
+{
+    public class JobsPage
+    {
+        public readonly string TestJobName;
+        public readonly Func<AppQuery, AppQuery> JobsListView = e => e.Marked("jobsListView");
+        public readonly Func<AppQuery, AppQuery> TestJob;
+
+        public JobsPage(string testJobName)
+        {
+            TestJobName = testJobName;
+            TestJob = e => e.Text(TestJobName);
+        }
+    }
+}
--- Mobile/UITests/Tests.cs
+++ Mobile/UITests/Tests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using NUnit.Framework;
 using Xamarin.UITest;
 using Xamarin.UITest.Queries;
+using ContosoFieldService.UITests.PageObjects;
 
 namespace ContosoFieldService.UITests
 {
@@ -14,9 +15,18 @@ namespace ContosoFieldService.UITests
         IApp app;
         Platform platform;
 
+        string testJobName = "Paint Boeing 737";
+
+        LoginPage loginPage;
+        JobsPage jobsPage;
+        JobsDetailsPage jobsDetailsPage;
+
         public Tests(Platform platform)
         {
             this.platform = platform;
+            this.loginPage = new LoginPage();
+            this.jobsPage = new JobsPage(testJobName);
+            this.jobsDetailsPage = new JobsDetailsPage();
         }
 
         [SetUp]
@@ -26,12 +36,35 @@ namespace ContosoFieldService.UITests
         }
 
         [Test]
-        public void WelcomeTextIsDisplayed()
+        public void ShowLoginPage()
         {
-            AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Xamarin Forms!"));
-            app.Screenshot("Welcome screen.");
+            app.WaitForElement(loginPage.ProceedButton);
+            app.Screenshot("Login page shown.");
+        }
 
-            Assert.IsTrue(results.Any());
+        [Test]
+        public void NavigateThroughJobs()
+        {
+            // Skip login
+            app.WaitForElement(loginPage.ProceedButton);
+            app.Tap(loginPage.ProceedButton);
+
+            // Show list of Jobs
+            app.WaitForElement(jobsPage.JobsListView);
+            app.Screenshot("Jobs list shown.");
+
+            // Scroll down to Paint Boeing 737 job
+            app.ScrollDownTo(jobsPage.TestJob);
+            app.Screenshot("Paint Boeing 737 job shown.");
+
+            // Open job
+            app.Tap(jobsPage.TestJob);
+            app.WaitForElement(jobsDetailsPage.JobName);
+            app.Screenshot("Job Details shown.");
+
+            // Check if displayed name is correct
+            var jobNameText = app.Query(jobsDetailsPage.JobName).First().Text;
+            Assert.AreEqual(jobNameText, testJobName);
         }
     }
 }
