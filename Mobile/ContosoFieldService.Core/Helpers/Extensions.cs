3be4677db1d75feeeedafcee8f25b12874c39f5d
Mike James <mike@xamarin.com>
2018-06-22 15:00:48 +0200

    First draft of new hit-highlighting implementation

--- Mobile/ContosoFieldService.Core/Helpers/Extensions.cs
+++ Mobile/ContosoFieldService.Core/Helpers/Extensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Linq;
 using ContosoFieldService.Models;
 using Xamarin.Forms;
 
@@ -27,8 +28,109 @@ namespace ContosoFieldService.Helpers
 
         public static FormattedString ConvertNameToFormattedString(this Job job)
         {
+            return NewConvertNameToFormattedString(job);
+        }
+
+        private static FormattedString NewConvertNameToFormattedString(Job job)
+        {
+            FormattedString formattedString = new FormattedString();
+
+            //The job name has a hit!
+            if(job.Name.Contains("[") && job.Name.Contains("]"))
+            {
+                //Copy job name 
+                var name = job.Name;
+                Console.WriteLine($"Job Name: {name}");
+
+                //Get the stand and end index position in string of the area to bold. 
+                var highlightPosition = PositionOfHighlight(name);
+
+                //Length of the text to highlight minus the brakets 
+                var lengthOfPreHighlight = highlightPosition.Item1;
+                var lengthOfHighlight = highlightPosition.Item2 - highlightPosition.Item1 +1;
+                var lengthOfPostHighlight = name.Length - (lengthOfPreHighlight + lengthOfHighlight);
+
+                //Remove the brackets from name 
+                name = name.Remove(highlightPosition.Item1, 1);
+                name = name.Remove(highlightPosition.Item2 + 1, 1);
+
+                Console.WriteLine($"Removed brackets and left with : {name}");
+
+                if (highlightPosition.Item1 == 0)
+                {
+                    //The match is at the start of the string so we can assume we'll only have 2 substrings. One to bold and the other to remain normal
+                    var matchedString = name.Substring(highlightPosition.Item1, lengthOfHighlight);
+                    var postMatchString = name.Substring(highlightPosition.Item2 + 1);
+
+                    formattedString.Spans.Add(new Span { Text = matchedString, FontAttributes = FontAttributes.Bold });
+                    formattedString.Spans.Add(new Span { Text = postMatchString });
+                }
+                else if(highlightPosition.Item2 == name.Length - 1)
+                {
+                    //The match extends to the last character so we'll only need to split into two again
+
+                    //The match is at the start of the string so we can assume we'll only have 2 substrings. One to bold and the other to remain normal
+                    var preMatchString = name.Substring(0, highlightPosition.Item1);
+                    var matchedString = name.Substring(highlightPosition.Item1, lengthOfHighlight);
+
+                    formattedString.Spans.Add(new Span { Text = preMatchString });
+                    formattedString.Spans.Add(new Span { Text = matchedString, FontAttributes = FontAttributes.Bold });
+                }
+                else
+                {
+                    // The String will need to be split into three sections as it'll have the pre-highlight, highlight and post-highlight text
+                    var preMatchString = name.Substring(0, highlightPosition.Item1);
+                    var matchedString = name.Substring(highlightPosition.Item1, lengthOfHighlight);
+                    var postMatchString = name.Substring(highlightPosition.Item2 + 1);
+
+                    formattedString.Spans.Add(new Span { Text = preMatchString });
+                    formattedString.Spans.Add(new Span { Text = matchedString, FontAttributes = FontAttributes.Bold });
+                    formattedString.Spans.Add(new Span { Text = postMatchString });
+                }
+
+                //Lets just check that we've created something...
+                if(formattedString.Spans.Count == 0)
+                    formattedString = new FormattedString { Spans = { new Span { Text = name } } }; 
+
+            }
+            else
+            {
+                formattedString = new FormattedString { Spans = { new Span { Text = job.Name } } }; 
+            }
+
+            return formattedString;
+        }
+
+        private static Tuple<int, int> PositionOfHighlight(string name)
+        {
+            /* We want to get the index positions for creating bold whilst taking into account that we'll be
+             * removing the brackets which will effect the end position */
+
+            //Copy the contents of the string to a char array for finding the positions for splitting the string
+            var charArray = name.ToCharArray();
+
+            //Position of [ matched highlight 
+            var startOfHighlight = 0;
+            //Position of ] matched hightlight 
+            var endofHighlight = 0;
+
+            //Loop through the chars looking for hits.
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                var character = charArray[i];
+                if (character == char.Parse("["))
+                    startOfHighlight = i;
+
+                if (character == char.Parse("]"))
+                    endofHighlight = i - 2;
+            }
+            return Tuple.Create(startOfHighlight, endofHighlight);
+        }
+
+        private static FormattedString LegacyConvertNameToFormattedString(Job job)
+        {
             //We want to return a FormattedString which is a Xamarin.Forms Type that allows us to style text elements
-             var formattedString = new FormattedString(); 
+            var formattedString = new FormattedString();
 
             //We'll use regex to nd content between square brackets [contents]
             var regexPattern = @"\[(\w*)\]";
@@ -52,6 +154,7 @@ namespace ContosoFieldService.Helpers
                 foreach (Match match in patternMatches)
                 {
                     highlightedWords.Add(RemoveBrackets(match.Value));
+                    Console.WriteLine($"match : {match}");
                 }
 
                 //We split the name input parts based on our RegEx. "Hello [World]" would become an array of string containing two items, "Hello" and "World";
@@ -75,6 +178,7 @@ namespace ContosoFieldService.Helpers
         {
             var removedPreTag = input.Replace("[", "");
             var removedPostTag = removedPreTag.Replace("]", "");
+            var removedWhiteSpaceTag = removedPostTag.Remove(0);
             return removedPostTag;
         }
     }
