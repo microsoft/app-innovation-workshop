68d055ac2500ffb03119a7e7107abc667794c87e
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2018-08-24 15:28:48 +0100

    Define use of multiple appsettings files and sources

--- Backend/Monolithic/Startup.cs
+++ Backend/Monolithic/Startup.cs
@@ -18,12 +18,18 @@ namespace ContosoMaintenance.WebAPI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+               .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
