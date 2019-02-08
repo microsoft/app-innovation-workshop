25792b05295a93a0bbc9fc010587fc8c35cbb3e7
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2019-02-08 14:08:55 +0100

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,14 +6,14 @@ provider "azurerm" {
 # Follow the official guide, to setup the Storage Account:
 # https://docs.microsoft.com/en-us/azure/terraform/terraform-backend
 
-# terraform {
-#   backend "azurerm" {
-#     storage_account_name = "__TFSTATE-STORAGE-ACCOUNT-NAME__" # <-- Replace this with your values
-#     container_name       = "__TFSTATE-CONTAINER-NAME__"       # <-- Replace this with your values
-#     access_key           = "__TFSTATE-STORAGE-ACCESS-KEY__"   # <-- Replace this with your values
-#     key                  = "terraform.tfstate"
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    storage_account_name = "__TFSTATE-STORAGE-ACCOUNT-NAME__" # <-- Replace this with your values
+    container_name       = "__TFSTATE-CONTAINER-NAME__"       # <-- Replace this with your values
+    access_key           = "__TFSTATE-STORAGE-ACCESS-KEY__"   # <-- Replace this with your values
+    key                  = "terraform.tfstate"
+  }
+}
 
 resource "azurerm_resource_group" "workshop" {
   name     = "${var.prefix}${var.resource_name}workshop"
