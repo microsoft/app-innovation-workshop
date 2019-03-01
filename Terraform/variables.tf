79602ac6bb770680d16b0b03044efae5fb7c3d7b
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2019-03-01 13:41:11 +0100

    Commented aks out in TF

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -6,11 +6,3 @@ variable "resource_name" {
   default     = "contoso"
   description = "Name of the project, which will be attached to every resource name."
 }
-
-variable "sp_client_id" {
-  description = "Client ID of the Service Principal for AKS."
-}
-
-variable "sp_client_secret" {
-  description = "Password of the Service Principal for AKS."
-}
